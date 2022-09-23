d20a86c01c0ad9a635e72eac89aa2d2a74df2d42
Keshav Aggarwal <98839532+jovian-explorer@users.noreply.github.com>
2022-09-23 20:36:56 +0530

    Add files via upload

--- MOM/motion_sim.py
+++ MOM/motion_sim.py
@@ -5,16 +5,16 @@ import matplotlib.animation as animation
 from astropy.time import Time
 from astroquery.jplhorizons import Horizons
 
-sim_start_date = "2018-01-01"     # simulating a solar system starting from this date
-sim_duration = 2 * 365                # (int) simulation duration in days
+sim_start_date = "2021-01-01"     # simulating a solar system starting from this date
+sim_duration = 365            # (int) simulation duration in days
 m_earth = 5.9722e24 / 1.98847e30  # Mass of Earth relative to mass of the sun
 m_moon = 7.3477e22 / 1.98847e30
 
 class Object:                   # define the objects: the Sun, Earth, Mercury, etc
     def __init__(self, name, rad, color, r, v):
         self.name = name
-        self.r    = np.array(r, dtype=np.float)
-        self.v    = np.array(v, dtype=np.float)
+        self.r    = np.array(r, dtype=float)
+        self.v    = np.array(v, dtype=float)
         self.xs = []
         self.ys = []
         self.plot = ax.scatter(r[0], r[1], color=color, s=rad**2, edgecolors=None, zorder=10)
@@ -59,7 +59,7 @@ sizes = [0.38, 0.95, 1., 0.53]
 names = ['Mercury', 'Venus', 'Earth','Moon', 'Mars']
 texty = [.47, .73, 1, 1.5]
 for i, nasaid in enumerate([1, 2, 3, 4]):  # The 1st, 2nd, 3rd, 4th planet in solar system
-    obj = Horizons(id=nasaid, location="@sun", epochs=ss.time, id_type='id').vectors()
+    obj = Horizons(id=nasaid, location="@sun", epochs=ss.time, id_type=None).vectors()
     ss.add_planet(Object(nasaid, 20 * sizes[i], colors[i], 
                          [np.double(obj[xi]) for xi in ['x', 'y', 'z']], 
                          [np.double(obj[vxi]) for vxi in ['vx', 'vy', 'vz']]))
