aac02a1129d6cfc896f857f451826d94d5d63d05
Keshav Aggarwal <98839532+jovian-explorer@users.noreply.github.com>
2023-04-24 20:05:41 +0530

    Update vel.py

--- MOM/Code/50yearplot/vel.py
+++ MOM/Code/50yearplot/vel.py
@@ -6,12 +6,13 @@ import matplotlib
 from matplotlib import pyplot as plt
 import warnings
 warnings.filterwarnings("ignore")
-fig,ax = plt.subplots()
-#fig,ax = plt.subplots(figsize = (20,40),dpi = 1200)
 # -----------------------------------------------------------------------------------------------------------------
+
+#figsize = (20,40),
+fig,ax = plt.subplots(figsize = (20,20),dpi =80)
 # -----------------------------------------------------------------------------------------------------------------
 #**Parker_1958**
-Parker_1958 = '/home/dev/Sun/velocity/curve_1.csv'
+Parker_1958 = 'https://raw.githubusercontent.com/jovian-explorer/Sun/main/MOM/Code/50yearplot/curve_1.csv'
 Parker = pd.read_csv(Parker_1958,delimiter = ',')
 Parker.columns = ["x","y"]
 RAD_Parker = np.array(Parker.x)
@@ -19,7 +20,7 @@ VEL_Parker = np.array(Parker.y)
 plt.loglog(RAD_Parker,VEL_Parker,label = "Curve 1")
 # -----------------------------------------------------------------------------------------------------------------
 #**curve_2**
-curve_2 = '/home/dev/Sun/velocity/curve_2.csv'
+curve_2 = 'https://raw.githubusercontent.com/jovian-explorer/Sun/main/MOM/Code/50yearplot/curve_2.csv'
 curve__2 = pd.read_csv(curve_2,delimiter = ',')
 curve__2.columns = ["x","y"]
 RAD_curve__2 = np.array(curve__2.x)
@@ -27,7 +28,7 @@ VEL_curve__2 = np.array(curve__2.y)
 plt.loglog(RAD_curve__2,VEL_curve__2,label = "Curve 2")
 # -----------------------------------------------------------------------------------------------------------------
 #**Parker_1958**
-curve__3 = '/home/dev/Sun/velocity/curve_3.csv'
+curve__3 = 'https://raw.githubusercontent.com/jovian-explorer/Sun/main/MOM/Code/50yearplot/curve_3.csv'
 curve_3 = pd.read_csv(curve__3,delimiter = ',')
 curve_3.columns = ["x","y"]
 RAD_curve_3 = np.array(curve_3.x)
@@ -36,7 +37,7 @@ plt.loglog(RAD_curve_3,VEL_curve_3,label = "Curve 3")
 # -----------------------------------------------------------------------------------------------------------------
 # -----------------------------------------------------------------------------------------------------------------
 #**Ekers_Little_1971**
-ekers_1971 = '/home/dev/Sun/velocity/ekers_little_1971.csv'
+ekers_1971 = 'https://raw.githubusercontent.com/jovian-explorer/Sun/main/MOM/Code/50yearplot/ekers_little_1971.csv'
 ekers = pd.read_csv(ekers_1971,delimiter = ',')
 ekers.columns = ["x","y"]
 RAD_ekers = np.array(ekers.x)
@@ -46,7 +47,7 @@ plt.plot(RAD_ekers,VEL_ekers,'v',label="Ekers_Little_1971")
 plt.scatter(1.7,24,label="Woo_1978")
 # -----------------------------------------------------------------------------------------------------------------
 #**BRADFORD & ROUTLEDGE 1980**
-bradford_1980 = '/home/dev/Sun/velocity/bradford_routledge_1980.csv'
+bradford_1980 = 'https://raw.githubusercontent.com/jovian-explorer/Sun/main/MOM/Code/50yearplot/bradford_routledge_1980.csv'
 bradford = pd.read_csv(bradford_1980,delimiter = ',')
 bradford.columns = ["x","y"]
 RAD_bradford = np.array(bradford.x)
@@ -54,7 +55,7 @@ VEL_bradford = np.array(bradford.y)
 plt.plot(RAD_bradford,VEL_bradford,'^',label="Bradford_Routledge_1980")
 # -----------------------------------------------------------------------------------------------------------------
 #**ARMSTRONG_WOO 1981**
-armstrong_1981 = '/home/dev/Sun/velocity/armstrong_woo_1981.csv'
+armstrong_1981 = 'https://raw.githubusercontent.com/jovian-explorer/Sun/main/MOM/Code/50yearplot/armstrong_woo_1981.csv'
 armstrong1 = pd.read_csv(armstrong_1981,delimiter = ',')
 armstrong1.columns = ["x","y"]
 RAD_armstrong_1981 = np.array(armstrong1.x)
@@ -62,7 +63,7 @@ VEL_armstrong_1981 = np.array(armstrong1.y)
 plt.plot(RAD_armstrong_1981,VEL_armstrong_1981,'d',label="Armstrong_Woo_1981")
 # -----------------------------------------------------------------------------------------------------------------
 #**EFIMOV 1981**
-efimov_1981 = '/home/dev/Sun/velocity/efimov_1981.csv'
+efimov_1981 = 'https://raw.githubusercontent.com/jovian-explorer/Sun/main/MOM/Code/50yearplot/efimov_1981.csv'
 efimov = pd.read_csv(efimov_1981,delimiter = ',')
 efimov.columns = ["x","y"]
 RAD_efimov = np.array(efimov.x)
@@ -70,7 +71,7 @@ VEL_efimov = np.array(efimov.y)
 plt.plot(RAD_efimov,VEL_efimov,'s',label="Efimov_1981")
 # -----------------------------------------------------------------------------------------------------------------
 #**tyler_1981**
-tyler_1981 = '/home/dev/Sun/velocity/tyler_1981.csv'
+tyler_1981 = 'https://raw.githubusercontent.com/jovian-explorer/Sun/main/MOM/Code/50yearplot/tyler_1981.csv'
 tyler = pd.read_csv(tyler_1981,delimiter = ',')
 tyler.columns = ["x","y"]
 RAD_tyler = np.array(tyler.x)
@@ -78,7 +79,7 @@ VEL_tyler = np.array(tyler.y)
 plt.plot(RAD_tyler,VEL_tyler,'p',label="Tyler_1981")
 # -----------------------------------------------------------------------------------------------------------------
 #**scott_1983**
-scott_1983 = '/home/dev/Sun/velocity/scott_1983.csv'
+scott_1983 = 'https://raw.githubusercontent.com/jovian-explorer/Sun/main/MOM/Code/50yearplot/scott_1983.csv'
 scott = pd.read_csv(scott_1983,delimiter = ',')
 scott.columns = ["x","y"]
 RAD_scott = np.array(scott.x)
@@ -86,7 +87,7 @@ VEL_scott = np.array(scott.y)
 plt.plot(RAD_scott,VEL_scott,'*',label="Scott_1983")
 # -----------------------------------------------------------------------------------------------------------------
 #**BOURGOIS 1985**
-bourgois_1985 = '/home/dev/Sun/velocity/bourgois_1985.csv'
+bourgois_1985 = 'https://raw.githubusercontent.com/jovian-explorer/Sun/main/MOM/Code/50yearplot/bourgois_1985.csv'
 bourgois = pd.read_csv(bourgois_1985,delimiter = ',')
 bourgois.columns = ["x","y"]
 RAD_bourgois = np.array(bourgois.x)
@@ -94,7 +95,7 @@ VEL_bourgois = np.array(bourgois.y)
 plt.scatter(RAD_bourgois,VEL_bourgois,label="Bourgois_1985")
 # -----------------------------------------------------------------------------------------------------------------
 #**ARMSTRONG 1986**
-armstrong_1986 = '/home/dev/Sun/velocity/armstrong_1986.csv'
+armstrong_1986 = 'https://raw.githubusercontent.com/jovian-explorer/Sun/main/MOM/Code/50yearplot/armstrong_1986.csv'
 armstrong = pd.read_csv(armstrong_1986,delimiter = ',')
 armstrong.columns = ["x","y"]
 RAD_armstrong_1986 = np.array(armstrong.x)
@@ -102,7 +103,7 @@ VEL_armstrong_1986 = np.array(armstrong.y)
 plt.scatter(RAD_armstrong_1986,VEL_armstrong_1986,label="Armstrong_1986")
 # -----------------------------------------------------------------------------------------------------------------
 #**yabukov_1991**
-yabukov_1991 = '/home/dev/Sun/velocity/yabukov_1991.csv'
+yabukov_1991 = 'https://raw.githubusercontent.com/jovian-explorer/Sun/main/MOM/Code/50yearplot/yabukov_1991.csv'
 data_yabukov_1991 = pd.read_csv(yabukov_1991,delimiter = ',')
 data_yabukov_1991.columns = ["x","y"]
 RAD_yabukov_1991 = np.array(data_yabukov_1991.x)
@@ -110,7 +111,7 @@ VEL_yabukov_1991 = np.array(data_yabukov_1991.y)
 plt.scatter(RAD_yabukov_1991,VEL_yabukov_1991,label="Yabukov_1991")
 # -----------------------------------------------------------------------------------------------------------------
 #**efimov_1994**
-efimov_1994 = '/home/dev/Sun/velocity/efimov_1994.csv'
+efimov_1994 = 'https://raw.githubusercontent.com/jovian-explorer/Sun/main/MOM/Code/50yearplot/efimov_1994.csv'
 data_efimov_1994 = pd.read_csv(efimov_1994,delimiter = ',')
 data_efimov_1994.columns = ["x","y"]
 RAD_efimov_1994 = np.array(data_efimov_1994.x)
@@ -118,7 +119,7 @@ VEL_efimov_1994 = np.array(data_efimov_1994.y)
 plt.scatter(RAD_efimov_1994,VEL_efimov_1994,label="Efimov_1994")
 # -----------------------------------------------------------------------------------------------------------------
 #**efimov_2002**
-efimov_2002 = '/home/dev/Sun/velocity/efimov_2002.csv'
+efimov_2002 = 'https://raw.githubusercontent.com/jovian-explorer/Sun/main/MOM/Code/50yearplot/efimov_2002.csv'
 data_efimov_2002 = pd.read_csv(efimov_2002,delimiter = ',')
 data_efimov_2002.columns = ["x","y"]
 RAD_efimov_2002 = np.array(data_efimov_2002.x)
@@ -126,7 +127,7 @@ VEL_efimov_2002 = np.array(data_efimov_2002.y)
 plt.scatter(RAD_efimov_2002,VEL_efimov_2002,label="Efimov_2002")
 # -----------------------------------------------------------------------------------------------------------------
 #**efimov_2010**
-efimov_2010 = '/home/dev/Sun/velocity/efimov_2010.csv'
+efimov_2010 = 'https://raw.githubusercontent.com/jovian-explorer/Sun/main/MOM/Code/50yearplot/efimov_2010.csv'
 data_efimov_2010 = pd.read_csv(efimov_2010,delimiter = ',')
 data_efimov_2010.columns = ["x","y"]
 RAD_efimov_2010 = np.array(data_efimov_2010.x)
@@ -134,7 +135,7 @@ VEL_efimov_2010 = np.array(data_efimov_2010.y)
 plt.scatter(RAD_efimov_2010,VEL_efimov_2010,label="Efimov_2010")
 # -----------------------------------------------------------------------------------------------------------------
 #**Imamura_2014**
-Imamura_2014 = '/home/dev/Sun/velocity/Imamura_2014.csv'
+Imamura_2014 = 'https://raw.githubusercontent.com/jovian-explorer/Sun/main/MOM/Code/50yearplot/Imamura_2014.csv'
 data_Imamura_2014 = pd.read_csv(Imamura_2014,delimiter = ',')
 data_Imamura_2014.columns = ["x","y"]
 RAD_Imamura_2014 = np.array(data_Imamura_2014.x)
@@ -142,7 +143,7 @@ VEL_Imamura_2014 = np.array(data_Imamura_2014.y)
 plt.scatter(RAD_Imamura_2014,VEL_Imamura_2014,label="Imamura_2014")
 # -----------------------------------------------------------------------------------------------------------------
 #**deforest_2018**
-deforest_2018 = '/home/dev/Sun/velocity/deforest_2018.csv'
+deforest_2018 = 'https://raw.githubusercontent.com/jovian-explorer/Sun/main/MOM/Code/50yearplot/deforest_2018.csv'
 data_deforest_2018 = pd.read_csv(deforest_2018,delimiter = ',')
 data_deforest_2018.columns = ["x","y"]
 RAD_deforest_2018 = np.array(data_deforest_2018.x)
@@ -150,7 +151,7 @@ VEL_deforest_2018 = np.array(data_deforest_2018.y)
 plt.scatter(RAD_deforest_2018,VEL_deforest_2018,label="Deforest_2018")
 # -----------------------------------------------------------------------------------------------------------------
 #**efimov_2018**
-efimov_2018 = '/home/dev/Sun/velocity/efimov_2018.csv'
+efimov_2018 = 'https://raw.githubusercontent.com/jovian-explorer/Sun/main/MOM/Code/50yearplot/efimov_2018.csv'
 data_efimov_2018 = pd.read_csv(efimov_2018,delimiter = ',')
 data_efimov_2018.columns = ["x","y"]
 RAD_efimov_2018 = np.array(data_efimov_2018.x)
@@ -158,7 +159,7 @@ VEL_efimov_2018 = np.array(data_efimov_2018.y)
 plt.scatter(RAD_efimov_2018,VEL_efimov_2018,label="Efimov_2018")
 # -----------------------------------------------------------------------------------------------------------------
 #**wexler_2020**
-wexler_2020 = '/home/dev/Sun/velocity/wexler_2020.csv'
+wexler_2020 = 'https://raw.githubusercontent.com/jovian-explorer/Sun/main/MOM/Code/50yearplot/wexler_2020.csv'
 data_wexler_2020 = pd.read_csv(wexler_2020,delimiter = ',')
 data_wexler_2020.columns = ["x","y"]
 RAD_wexler_2020 = np.array(data_wexler_2020.x)
@@ -166,7 +167,7 @@ VEL_wexler_2020 = np.array(data_wexler_2020.y)
 plt.plot(RAD_wexler_2020,VEL_wexler_2020,'+',label="Wexler_2020")
 # -----------------------------------------------------------------------------------------------------------------
 #**PSP**
-PSP_2022 = '/home/dev/Sun/velocity/PSP_2022.csv'
+PSP_2022 = 'https://raw.githubusercontent.com/jovian-explorer/Sun/main/MOM/Code/50yearplot/PSP_2022.csv'
 PSP = pd.read_csv(PSP_2022,delimiter = '\t')
 PSP.columns = ["x","y"]
 RAD_PSP = np.array(PSP.x)
@@ -174,7 +175,7 @@ VEL_PSP = np.array(PSP.y)
 plt.plot(RAD_PSP*215,VEL_PSP,'x',label="PSP_2022")
 # -----------------------------------------------------------------------------------------------------------------
 #**Chiba Akatsuki 2022**
-Chiba_Akatsuki_2022 = '/home/dev/Sun/velocity/Chiba_Akatsuki_2022.csv'
+Chiba_Akatsuki_2022 = 'https://raw.githubusercontent.com/jovian-explorer/Sun/main/MOM/Code/50yearplot/Chiba_Akatsuki_2022.csv'
 Chiba_Akatsuki = pd.read_csv(Chiba_Akatsuki_2022,delimiter = ',')
 Chiba_Akatsuki.columns = ["x","y"]
 RAD_Chiba_Akatsuki = np.array(Chiba_Akatsuki.x)
@@ -185,17 +186,18 @@ plt.plot(RAD_Chiba_Akatsuki,VEL_Chiba_Akatsuki,'+',label="Chiba_Akatsuki_2022")
 rad_MOM_RS = (8.1,6.92,5.77,5.24,6.33,7.49)
 rad_MOM_AU = (0.0376682,0.03218077,0.0268328,0.024368,0.0294370387,0.034831504)
 MOM = (172.2372178,112.83713799,126.64897552,189.53094138,212.84972353,260.6076649290)
-c = [0.25*172.2372178,0.25*112.83713799,0.25*126.64897552,0.25*189.53094138,0.25*212.84972353,0.25*260.6076649290]
-#plt.plot(rad_MOM_RS,MOM,'kX',label="MOM_2021")
-plt.plot(rad_MOM_RS,MOM,yerr = 0.25)
+err= (0.2*172.2372178,0.2*112.83713799,0.2*126.64897552,0.2*189.53094138,0.2*212.84972353,0.2*260.6076649290)
+plt.plot(rad_MOM_RS,MOM,'ko',label="MOM_2021")
+plt.errorbar(rad_MOM_RS,MOM, yerr=err,fmt='o',color = 'k')
 #------------------------------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------------------------
 #plt.scale("log")
 plt.ylabel("SOLAR WIND VELOCITY $(km/s) \longrightarrow$")
 plt.xlabel("SOLAR DISTANCE (in $R_{\odot}$) $\longrightarrow$ ")
-#plt.legend(ncol = 4,loc='lower center',bbox_to_anchor=(0.5, -2))
-plt.legend(bbox_to_anchor=(-0.2, 1.02, 1.2, .102), loc=3,ncol=3, mode="expand", borderaxespad=0.)
-plt.tight_layout()
+
+#plt.legend(ncol=1, loc='center right', bbox_to_anchor=(0.9, 0.25))
+plt.legend(ncol=1, loc='center right', bbox_to_anchor=(1.3,0.5))
+# plt.tight_layout()
 plt.ylim(20,1000)
 plt.xlim(1,100)
 ax.set_aspect('equal', adjustable='box')
